d1b657789347c595cc78457e8e78f3df840f043b
ell3n <mfernandantas@gmail.com>
2026-06-05 08:54:58 -0300

    aula 05/06- manha

--- Assets/scripts/PersonagemController.cs
+++ Assets/scripts/PersonagemController.cs
@@ -6,20 +6,26 @@ public class PersonagemController : MonoBehaviour
 {
     public Rigidbody2D rb2d;
     public float vel;
-    // Start is called before the first frame update
+    public float jumpForce;
+    public GameObject groundCheck;
+
     void Start()
     {
-        rb2d=this.GetComponent<Rigidbody2D>();
+        rb2d = this.GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         float horizontalInput = Input.GetAxis("Horizontal");
-        if(rb2d.velocity.magnitude < 5){
-            rb2d.velocity +=  new Vector2(vel,0) * horizontalInput*Time.deltaTime;
+        if (rb2d.velocity.magnitude < 5)
+        {
+            rb2d.velocity += new Vector2(vel, 0) * horizontalInput * Time.deltaTime;
         }
-      
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rb2d.velocity = new Vector2(rb2d.velocity.x, jumpForce);
+        }
 
     }
 }
