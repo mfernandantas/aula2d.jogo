38fc299791ad5abdf19a9efad0f2bf12d72c1a36
llucas <mfernandantas@gmail.com>
2026-06-26 16:16:11 -0300

    new cenario

--- Assets/Scripts/CoinController.cs
+++ Assets/Scripts/CoinController.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 
 public class CoinController : MonoBehaviour
 {
+    public GameObject coinParticle;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
             Pontuacao pontuacao = other.GetComponent<Pontuacao>();
             pontuacao.AddPoints();
+            Instantiate(coinParticle, this.transform.position, Quaternion.Euler(-90,0,0));
             Destroy(this.gameObject);
         }
     }
